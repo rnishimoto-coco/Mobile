6d1b1f0f9a29f2a158e72d6e3e3421d9bce17fba
rnishimoto-coco <123221087+rnishimoto-coco@users.noreply.github.com>
2023-01-22 10:44:01 +0900

    Add files via upload

---
+++ STEP3_Mobile_kitti/script.js
@@ -0,0 +1,108 @@
+$(function(){
+
+    let localStream = null;
+    let peer = null;
+    let existingCall = null;
+    let constraints = {
+        video: {
+            facingMode:{
+                exact: 'environment'
+            }
+        },
+        audio: true
+        
+    };
+    constraints.video.width = {
+        min: 1216/2,
+        max: 1216/2
+    };
+    constraints.video.height = {
+        min: 352/2,
+        max: 352/2        
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(function (stream) {
+            $('#myStream').get(0).srcObject = stream;
+            localStream = stream;
+        }).catch(function (error) {
+            console.error('mediaDevice.getUserMedia() error:', error);
+            return;
+        });
+
+    peer = new Peer({
+        key: 'eaef9e8b-d312-4086-92ef-1a3f81ebe9fe',
+        debug: 3
+    });
+
+    peer.on('open', function(){
+        $('#my-id').text(peer.id);
+    });
+
+    peer.on('error', function(err){
+        alert(err.message);
+    });
+
+    $('#make-call').submit(function(e){
+        e.preventDefault();
+        let roomName = $('#join-room').val();
+        if (!roomName) {
+            return;
+        }
+        const call = peer.joinRoom(roomName, {mode: 'sfu', stream: localStream});
+        setupCallEventHandlers(call);
+    });
+
+    $('#end-call').click(function(){
+        existingCall.close();
+    });
+
+    function setupCallEventHandlers(call){
+        if (existingCall) {
+            existingCall.close();
+        };
+
+        existingCall = call;
+        setupEndCallUI();
+        $('#room-id').text(call.name);
+
+        call.on('stream', function(stream){
+            addVideo(stream);
+        });
+
+        call.on('peerLeave', function(peerId){
+            removeVideo(peerId);
+        });
+
+        call.on('close', function(){
+            removeAllRemoteVideos();
+            setupMakeCallUI();
+        });
+    }
+
+    function addVideo(stream){
+        const videoDom = $('<video autoplay>');
+        videoDom.attr('id',stream.peerId);
+        videoDom.get(0).srcObject = stream;
+        $('.videosContainer').append(videoDom);
+    }
+
+    function removeVideo(peerId){
+        $('#'+peerId).remove();
+    }
+
+    function removeAllRemoteVideos(){
+        $('.videosContainer').empty();
+    }
+
+    function setupMakeCallUI(){
+        $('#make-call').show();
+        $('#end-call').hide();
+    }
+    
+    function setupEndCallUI() {
+        $('#make-call').hide();
+        $('#end-call').show();
+    }
+
+});
